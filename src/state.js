5ed8f257b849e1afaa2d60fbe7bc8780660a4838
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-12 19:12:05 +0800

    获取用户选项中的data数据

--- src/state.js
+++ src/state.js
@@ -1,5 +1,23 @@
+import {isFunction} from "./utils";
 
 export function initState(vm) { // 状态的初始化
     const opts = vm.$options;
     console.log("initSate",opts)
+    if (opts.data) {
+        initData(vm);
+    }
+    // if(opts.computed){
+    //     initComputed();
+    // }
+    // if(opts.watch){
+    //     initWatch();
+    // }
+}
+
+function initData(vm) { //
+    let data = vm.$options.data; // vm.$el  vue 内部会对属性检测如果是以$开头 不会进行代理
+    // vue2中会将data中的所有数据 进行数据劫持 Object.defineProperty
+    data = vm._data = isFunction(data) ? data.call(vm) : data;//data可能是函数或者对象
+    console.log("data",data)
+
 }
