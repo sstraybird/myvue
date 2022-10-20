68bf2f05262299ff45162e9fa64683ebcff8a231
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-21 00:33:12 +0800

    拼接子元素

--- src/compiler/generate.js
+++ src/compiler/generate.js
@@ -18,10 +18,30 @@ function genProps(attrs) { // [{name:'xxx',value:'xxx'},{name:'xxx',value:'xxx'}
     return `{${str.slice(0,-1)}}`
 }
 
+function gen(el) {
+    if (el.type == 1) { // element = 1 text = 3
+        return generate(el);
+    } else {
+        let text = el.text;
+        return `_v('${text}')`;
+    }
+}
+
+function genChildren(el) {
+    let children = el.children; // 获取儿子
+    if (children) {
+        return children.map(c => gen(c)).join(',')
+    }
+    return false;
+}
+
 export function generate(el) { //  _c('div',{id:'app',a:1},_c('span',{},'world'),_v())
     // 遍历树 将树拼接成字符串
+    let children = genChildren(el);
     let code = `_c('${el.tag}',${       //es6字符串
         el.attrs.length? genProps(el.attrs): 'undefined'
+    }${
+        children? `,${children}`:''
     })`;
 
     return code;
