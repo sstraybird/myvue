3cb3c897288fde9aab3650129e56c1cdc92bc78a
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-21 19:46:27 +0800

    生成虚拟节点框架

--- src/render.js
+++ src/render.js
@@ -1,6 +1,18 @@
 export function renderMixin(Vue){
-
+    Vue.prototype._c = function(){ // createElement
+        return console.log('_c arguments:',...arguments)
+    }
+    Vue.prototype._v = function (text) { // createTextElement
+        console.log('_v text:',text)
+    }
+    Vue.prototype._s = function(val){ // stringify
+        console.log('_s val:',val)
+    }
     Vue.prototype._render = function(){
         console.log('_render')
+        const vm = this;
+        let render =  vm.$options.render; // 就是我们解析出来的render方法，同时也有可能是用户写的
+        let vnode =  render.call(vm);
+        return vnode;
     }
 }
