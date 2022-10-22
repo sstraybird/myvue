5ed858a1a1728c1b25f7516b78f0d5c847875b8c
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-22 14:11:31 +0800

    数组更新

--- Readme.md
+++ Readme.md
@@ -7,3 +7,17 @@
 2、创建虚拟节点取vm中的数据，然后虚拟节点生成真实节点
 3、dep的作用：每个属性都要存着依赖收集的对象，不能直接存在属性上，要存在属性关联的属性上
 4、一个组件对应一个watcher，一个属性对应一个dep
+5、数组更新原理：数组本身有dep属性，当去取数组值时数组本身会把watcher存起来，当调用数组的push等方法，会把数组本身的watcher执行
+数组和对象都加dep
+observeData {arr: Array(3), age: 19}
+
+data111 {arr: Array(3), age: 19}
+
+{arr: Array(3), age: 19}.__ob__ = this
+
+observeData (3) [1, 2, 3]
+data111 (3) [1, 2, 3]
+
+[1,2,3].__ob__ = this
+
+this.dep = new Dep()
--- src/observer/index.js
+++ src/observer/index.js
@@ -6,15 +6,20 @@ import Dep from "./dep";
 // 2.如果是数组，会劫持数组的方法，并对数组中不是基本数据类型的进行检测
 
 // 检测数据变化 类有类型 ， 对象无类型
+
+// 如果给对象新增一个属性不会触发视图更新  (给对象本身也增加一个dep，dep中存watcher，如果增加一个属性后，我就手动的触发watcher的更新)
 class Observer {
     constructor(data) { // 对对象中的所有属性 进行劫持
         console.log('data111',data)
+
+        this.dep = new Dep(); // 数据可能是数组或者对象
+
         Object.defineProperty(data,'__ob__',{
             value:this,
             enumerable:false // 不可枚举的   防止walk循环到此属性从而出现死循环
         })
         // data.__ob__ = this; // 所有被劫持过的属性都有__ob__
-        if(Array.isArray(data)) {
+        if(Array.isArray(data)) {   // 我希望数组的变化可以触发视图更新？
             // 数组劫持的逻辑
             // 对数组原来的方法进行改写， 切片编程  高阶函数
             data.__proto__ = arrayMethods;
@@ -37,14 +42,18 @@ class Observer {
 
 // vue2 会对对象进行遍历 将每个属性 用defineProperty 重新定义 性能差
 function defineReactive(data,key,value){ // value有可能是对象
-    observe(value); // 本身用户默认值是对象套对象 需要递归处理 （性能差）
+    let childOb = observe(value); // 本身用户默认值是对象套对象 需要递归处理 （性能差）
     let dep = new Dep(); // 每个属性都有一个dep属性
+    console.log('childObj',childOb)        // 获取到了数组对应ob
     Object.defineProperty(data,key,{
         get(){
             console.log('get',data,key)
             // 取值时我希望将watcher和dep 对应起来
             if(Dep.target){ // 此值是在模板中取值的
                 dep.depend() // 让dep记住watcher
+                if(childOb) { // 可能是数组 可能是对象，对象也要收集依赖，后续写$set方法时需要触发他自己的更新操作
+                    childOb.dep.depend(); // 就是让数组和对象也记录watcher
+                }
             }
             console.log('--->',data,key,value,dep)
             return value
@@ -67,7 +76,7 @@ export function observe(data) {
         return;
     }
     if(data.__ob__){    //如果data已经被观测过就不用再观测了
-        return;
+        return data.__ob__;
     }
     // 默认最外层的data必须是一个对象
     console.log("observeData",data)
