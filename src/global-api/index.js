54b7a17d86825e5e7f4609463d0632597099f853
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-26 18:15:47 +0800

    全局组件创建

--- src/global-api/index.js
+++ src/global-api/index.js
@@ -14,4 +14,28 @@ export function initGlobalApi(Vue) {
         return this;
     }
 
+    Vue.options._base = Vue; // 无论后续创建多少个子类 都可以通过_base找到Vue
+    Vue.options.components = {};
+    Vue.component = function (id,definition){
+        // 保证组件的隔离， 每个组件都会产生一个新的类，去继承父类
+        definition = this.options._base.extend(definition);
+        console.log("definition:",definition)
+        this.options.components[id] = definition;
+        console.log("this.options.components",this.options.components)
+    }
+
+
+    // 给个对象返回类
+    Vue.extend = function (opts) { // extend方法就是产生一个继承于Vue的类
+        // 并且身上应该有父类的所有功能
+        const Super = this
+        const Sub = function VueComponent(){
+            this._init();        //将子组件变成render函数
+        }
+        // 原型继承
+        Sub.prototype = Object.create(Super.prototype);
+        Sub.prototype.constructor = Sub;
+        Sub.options = mergeOptions(Super.options,opts);// 只和Vue.options合并
+        return Sub;
+    }
 }
