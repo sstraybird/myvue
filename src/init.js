40bad5ff8a5cff640af91f09dc34f617b9a4e9d8
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-26 16:12:09 +0800

    Vue选项与minix接口中的选项合并

--- src/init.js
+++ src/init.js
@@ -3,13 +3,15 @@
 import {initState} from "./state";
 import {compileToFunction} from "./compiler/index";
 import {mountComponent} from "./lifecycle";
+import {mergeOptions} from "./utils";
 
 export function initMixin(Vue) {        //这里的Vue是VUe构造函数
     Vue.prototype._init = function(options) {
         console.log(options);
         const vm = this     //this 都是指原型的实例 就是在index.html new出的vm   这里赋值给vm方便阅读 var that = this;
-        vm.$options = options    // 后面会对options进行扩展操作
-
+        // vm.$options = options    // 后面会对options进行扩展操作
+        vm.$options = mergeOptions(vm.constructor.options, options); // 后面会对options进行扩展操作
+        console.log('vm.$options',vm.$options)
         // 对数据进行初始化 watch computed props data ...
         initState(vm); // vm.$options.data  数据劫持
 
